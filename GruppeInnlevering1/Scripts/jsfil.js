3bff536b2c7615a6657fdb61b1ca24630048233c
Ali Zedan <ali2016zedan@gmail.com>
2019-10-01 20:18:19 +0200

    hrar asdasd

--- GruppeInnlevering1/Scripts/jsfil.js
+++ GruppeInnlevering1/Scripts/jsfil.js
@@ -1,14 +1,24 @@
-﻿function canChangeColor() {
+﻿function buttonChangeColor() {
 
-    var can = true;
+    var button = true;
+    var stu = document.getElementsByName('student');
+    var vok = document.getElementsByName('voksen');
+    var bar = document.getElementsByName('barn');
+    var feil = document.getElementById('StasjonTil').value;
+    var feil2 = document.getElementById('st').value;
 
     $("input[type='text'], input").each(function () {
         if ($(this).val() == '') {
-            can = false;
+            button = false;
+        } else if ($(stu).val() == '0' && $(vok).val() == '0' && $(bar).val() == '0') {
+            button = false;
+        } 
+        else if (feil == feil2){
+            button = false;
         }
     });
 
-    if (can) {
+    if (button) {
         $('.btn').css({ background: 'darkseagreen' })
     }
 
